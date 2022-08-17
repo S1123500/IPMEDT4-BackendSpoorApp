a4d7ba2d230319aefd57f8d53a04448bd9c451b7
SPR1NG090 <gijs.van.vugt8@gmail.com>
2022-08-17 19:16:52 +0200

    /locaties post en get

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
     return view('form');
 });
 
-Route::post('/locatie','App\Http\Controllers\LocatieController@store');
+Route::post('/locaties','App\Http\Controllers\LocatieController@store');
 
 Route::get('/insert','App\Http\Controllers\InsertController@insert');
 Route::post('/create','App\Http\Controllers\InsertController@create');
